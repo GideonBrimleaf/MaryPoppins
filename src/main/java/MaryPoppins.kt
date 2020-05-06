84bd831583c9f67e8daebb5180f5d6855bf9f581
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-06 11:27:12 +0100

    adds css

--- src/main/java/MaryPoppins.kt
+++ src/main/java/MaryPoppins.kt
@@ -2,7 +2,7 @@ import io.javalin.Javalin
 
 fun main(){
     val app = Javalin.create {
-//        it.addStaticFiles("/public")
+        it.addStaticFiles("/public")
     }.start(7000)
 
     app.routes {
