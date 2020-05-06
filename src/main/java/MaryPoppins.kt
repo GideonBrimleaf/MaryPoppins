6c59b20167f8bb4d53b12512d2db3f7f0cbad1bc
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-06 11:32:57 +0100

    adds templates

--- src/main/java/MaryPoppins.kt
+++ src/main/java/MaryPoppins.kt
@@ -6,6 +6,9 @@ fun main(){
     }.start(7000)
 
     app.routes {
-        app.get("/") { ctx -> ctx.result("I'm Mary Poppins Y'All!") }
+        app.get("/") { ctx -> ctx.render("templates/index.peb", mapOf("message" to "I'm Mary Poppins Y'All!")) }
+        app.get("/speak-chewie") { ctx -> ctx.render("templates/speakChewie.peb") }
+        app.get("/textie") { ctx -> ctx.result("I'm Mary Poppins Y'All!") }
+        app.get("/jsonarama") {ctx -> ctx.json(mapOf("data" to "I'm Mary Poppins Y'All"))}
     }
 }
