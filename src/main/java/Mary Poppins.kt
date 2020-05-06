231eb0991a706db9da109f3f940c6cf26c36cc2a
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-06 11:14:22 +0100

    initial commit

---
+++ src/main/java/Mary Poppins.kt
@@ -0,0 +1,11 @@
+import io.javalin.Javalin
+
+fun main(){
+    val app = Javalin.create {
+//        it.addStaticFiles("/public")
+    }.start(7000)
+
+    app.routes {
+        app.get("/") { ctx -> ctx.result("I'm Mary Poppins Y'All!") }
+    }
+}
